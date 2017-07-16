76236766c7721eb4f285ceb78c5fedf543ded761
DESKTOP-DEC0EEN\Salih <salihgungor13@hotmail.com>
2017-07-16 14:24:33 +0300

    servis yazılmaya başlandı.

---
+++ CRUD with Rest Api/WebApi/WebApi/Controllers/ArticleController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    public class ArticleController : ApiController
+    {
+        private BlogEntities db = new BlogEntities();
+
+        // GET: api/Article
+        public List<Makale> GetMakale()
+        {
+            return db.Makale.ToList();
+        }
+
+        // GET: api/Article/5
+        [ResponseType(typeof(Makale))]
+        public IHttpActionResult GetMakale(int id)
+        {
+            Makale makale = db.Makale.Find(id);
+            if (makale == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(makale);
+        }
+
+        // PUT: api/Article/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutMakale(string id, Makale makale)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != makale.Baslik)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(makale).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MakaleExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Article
+        [ResponseType(typeof(Makale))]
+        public IHttpActionResult PostMakale(Makale makale)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Makale.Add(makale);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (MakaleExists(makale.Baslik))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return Json("Basarili");
+        }
+
+        // DELETE: api/Article/5
+        [ResponseType(typeof(Makale))]
+        public IHttpActionResult DeleteMakale(string id)
+        {
+            Makale makale = db.Makale.Find(id);
+            if (makale == null)
+            {
+                return NotFound();
+            }
+
+            db.Makale.Remove(makale);
+            db.SaveChanges();
+
+            return Ok(makale);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool MakaleExists(string id)
+        {
+            return db.Makale.Count(e => e.Baslik == id) > 0;
+        }
+    }
+}
---
+++ CRUD with Rest Api/WebApi/WebApi/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    public class HomeController : ApiController
+    {
+       private BlogEntities db = new BlogEntities();
+
+        [HttpGet]
+        public List<Makale> GetAllMakale()
+        {
+            return db.Makale.ToList();
+        }
+
+        [HttpPost]
+        public IHttpActionResult AddMakale(Makale model)
+        {
+            db.Makale.Add(model);
+            db.SaveChanges();
+
+            return Json("basarili");
+        }
+      
+    }
+}
